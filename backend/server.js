7fbce6b3f455b70d4826e178d9da2a5933e5e83c
Ramadan006 <ramzancoder2006@gmail.com>
2025-04-25 08:17:57 +0500

    Removed useless lines

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
 const port = 3000;
 const userRoute = require("./routes/userRoute");
 const projectRoute = require("./routes/projectRoute");
 
 app.use(express.json());
-// app.use(cors());
 
 app.use("/auth", userRoute);
 app.use("/project", projectRoute);
